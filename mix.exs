c2cc12645d62021c73b5477093e7f621ea273dc8
Nelson Estevão <nelsonmestevao@gmail.com>
2022-05-16 21:49:52 +0100

    Add documentation tools

--- mix.exs
+++ mix.exs
@@ -14,6 +14,7 @@ defmodule Icons.MixProject do
       description: @description,
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
+      docs: docs(),
       deps: deps()
     ]
   end
@@ -25,11 +26,24 @@ defmodule Icons.MixProject do
     ]
   end
 
+  defp docs do
+    [
+      main: "readme",
+      extras: [{:"README.md", [title: "Overview"]}],
+      source_ref: "v#{@version}"
+    ]
+  end
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:phoenix_html, "~> 3.2"},
-      {:phoenix_live_view, "~> 0.17.0"}
+      {:phoenix_live_view, "~> 0.17.0"},
+
+      # tools
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.1", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.28", only: [:dev], runtime: false}
     ]
   end
 end
